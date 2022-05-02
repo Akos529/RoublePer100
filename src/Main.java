4f9bf894c1f25c7767d93d5763e10e139eaed998
Taisia Ivanovskaya <taya.naumova@gmail.com>
2022-05-02 15:55:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialAmount = 100;
+        int paid = 100;
+        int bonus;
+
+        if (paid < 1000) {
+            bonus = 0;
+        } else {
+            bonus = paid / 100;
+        }
+        int finalAmount = initialAmount + paid + bonus;
+        System.out.println("Итоговая сумма на счете: " + finalAmount);
+        System.out.println("Начисленно бонусов: " + bonus);
+    }
+}
